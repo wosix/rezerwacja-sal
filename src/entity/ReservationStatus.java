7e21ee4facf22060971cb858af8820c4ed9b92c2
wosix <wojtekkk00@o2.pl>
2026-01-26 01:20:41 +0100

    Missing imports

--- src/entity/ReservationStatus.java
+++ src/entity/ReservationStatus.java
@@ -1,3 +1,5 @@
+package entity;
+
 public enum ReservationStatus {
     CANCELLED,
     PENDING,
--- src/entity/User.java
+++ src/entity/User.java
@@ -1,6 +1,12 @@
+package entity;
+
 public class User extends Account {
 
     public User() {
         setRole(Role.USER);
     }
+
+    private void setRole(Role role) {
+    }
+
 }
